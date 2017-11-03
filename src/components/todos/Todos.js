49713cb646d1ce8b6e3b6310e39019513dfc4616
Mariana Sickler <marianaleal_93@hotmail.com>
2017-11-03 23:15:48 +0000

    automatically backed up by learn

--- src/components/todos/Todos.js
+++ src/components/todos/Todos.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 class Todos extends Component {
 
   render() {
-   
+
       const todos = this.props.store.getState().todos.map((todo, index) => {
         return <Todo text={todo.text} key={index} /> /* code changed */
       });
-   
+
       return(
         <ul>
           {todos}
